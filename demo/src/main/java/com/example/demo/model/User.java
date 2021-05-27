83bf129a02901c25c127c13e86b337743a368fa7
forester186 <lesnaya186@gmail.com>
2021-05-27 21:09:04 +0500

    Commit

--- demo/src/main/java/com/example/demo/model/User.java
+++ demo/src/main/java/com/example/demo/model/User.java
@@ -12,7 +12,7 @@ import java.util.List;
 public class User implements UserDetails {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "name")
